74cd3332f25efeeba9f1e7a567d77d147fce5867
Bravin Thillainathan <89780339+bravint@users.noreply.github.com>
2022-03-23 06:31:35 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import 'dotenv/config';
-
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import testRouter from './routes/test';
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-
-import testRouter from './routes/test';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
